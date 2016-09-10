fad76b1d41d1478b191037ae7b65c4eff76236b4
Scott Miller <sam6258@psu.edu>
2016-09-10 18:34:27 -0400

    Remove unnecessary lines

--- typeform.php
+++ typeform.php
@@ -1,6 +1,4 @@
 <?
-$command = $_POST['command'];
-$text = $_POST['text'];
 $token = $_POST['token'];
 if($token != 'Df7GfPA1ud5KFPJTpM1Pm2PR'){ 
     $msg = "The token for the slash command doesn't match. Check your script.";
