90e8797ca8406d5d3691d542fa8073475d00d372
Scott Miller <sam6258@psu.edu>
2016-09-10 17:45:21 -0400

    Add response

--- typeform.php
+++ typeform.php
@@ -1,3 +1,4 @@
+<?
 $command = $_POST['command'];
 $text = $_POST['text'];
 $token = $_POST['token'];
@@ -6,6 +7,17 @@ if($token != 'Df7GfPA1ud5KFPJTpM1Pm2PR'){
     die($msg);
     echo $msg;
 }
-echo $command
-echo $text
-echo $token
+$ch = curl_init("https://api.typeform.com/v1/form/SMonEb?key=281c83336c6eb63b48c29d925e660adb1f950665&completed=true&offset=0&limit=50");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$ch_response = curl_exec($ch);
+curl_close($ch);
+
+if($ch_response === false){
+	$reply = "Typeform could not be reached.";
+}
+else{
+	$reply = $ch_response;
+}
+
+echo $reply
+>
